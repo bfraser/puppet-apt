67c800958d329aa24f945bdaf88d5b9d94e2ca73
Bill Fraser <fraser@pythian.com>
2011-12-17 22:43:26 -0500

    Added apt update exec to apt::source type, subscribing to source list

--- modules/apt/manifests/source.pp
+++ modules/apt/manifests/source.pp
@@ -2,15 +2,21 @@ define apt::source ($keys, $ensure) {
   include apt
 
   file { "/etc/apt/sources.list.d/${name}.list":
-    ensure => file,
-    source => "puppet:///modules/apt/sources/${name}.list",
-    owner => root,
-    group => root,
-    mode => 644,
-    notify => Exec[ Apt['/usr/bin/apt-get update'] ],
+    ensure	=> file,
+    source	=> "puppet:///modules/apt/sources/${name}.list",
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
   }
 
   apt::source::key { $keys:
-    ensure => $ensure,
+    ensure	=> $ensure,
+    before	=> File["/etc/apt/sources.list.d/${name}.list"],
+  }
+
+  exec { "${name} apt update":
+    command     => "${apt::get} update",
+    subscribe   => File["/etc/apt/sources.list.d/${name}.list"],
+    refreshonly => true,
   }
 }
