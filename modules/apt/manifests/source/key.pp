f76f46acc19f0651fb5cfa976e6529ffbefc1c0a
Bill Fraser <fraser@pythian.com>
2011-12-17 22:47:37 -0500

    Removed apt update from execs. Ensure key resource exists before adding to apt keys.

--- modules/apt/manifests/source/key.pp
+++ modules/apt/manifests/source/key.pp
@@ -2,26 +2,25 @@ define apt::source::key ($ensure) {
   include apt
 
   file { "/root/apt/keys/${keyid}.asc":
-    ensure => file,
-    source => "puppet:///modules/apt/keys/${keyid}.asc",
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure	=> file,
+    source	=> "puppet:///modules/apt/keys/${keyid}.asc",
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
+    before	=> Exec["apt-key present $name"],
   }
 
   case $ensure {
     present: {
       exec { "apt-key present $name":
-        command => "/usr/bin/apt-key add /root/apt/keys/${keyid}.asc",
-        unless => "/usr/bin/apt-key list | /bin/grep -c $keyid",
-        notify => Exec[ Apt["/usr/bin/apt-get update"] ],
+        command	=> "/usr/bin/apt-key add /root/apt/keys/${keyid}.asc",
+        unless	=> "/usr/bin/apt-key list | /bin/grep -c $keyid",
       }
     }
     absent: {
       exec { "apt-key absent $name":
         command => "/usr/bin/apt-key del $keyid",
-        onlyif => "/usr/bin/apt-key list | /bin/grep -c $keyid",
-        notify => Exec[ Apt["/usr/bin/apt-get update"] ],
+        onlyif	=> "/usr/bin/apt-key list | /bin/grep -c $keyid",
       }
     }
     default: {
