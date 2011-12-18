83627af2013763d99e3fe8a4bc10302d9f008917
Bill Fraser <fraser@pythian.com>
2011-12-17 22:39:46 -0500

    Added $get variable and modified update exec

--- modules/apt/manifests/init.pp
+++ modules/apt/manifests/init.pp
@@ -1,39 +1,39 @@
 class apt {
   $default_server = "us.archive.ubuntu.com"
+  $get = "/usr/bin/apt-get"
 
-  exec { '/usr/bin/apt-get update':
-    alias => 'aptgetupdate',
-    require => File['/etc/apt/sources.list'],
-    subscribe => File['/etc/apt/sources.list'],
+  exec { "$get update":
+    require	=> File['/etc/apt/sources.list'],
+    subscribe	=> File['/etc/apt/sources.list'],
     refreshonly => true,
   }
 
   file { '/etc/apt/sources.list':
-    ensure => file,
-    content => template("apt/sources.list.erb"),
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure	=> file,
+    content	=> template("apt/sources.list.erb"),
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
   }
 
   file { '/etc/apt/sources.list.d':
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure	=> directory,
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
   }
 
   file { "/root/apt":
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure	=> directory,
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
   }
 
   file { "/root/apt/keys":
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure	=> directory,
+    owner	=> root,
+    group	=> root,
+    mode	=> 644,
   }
 }
